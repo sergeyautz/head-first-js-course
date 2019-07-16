0f98d478eb60bf98d5cce021902d97fa4a3b2398
Sergey Autz <sergeyauts@gmail.com>
2019-07-16 16:16:35 +0300

    test

---
+++ script.js
@@ -0,0 +1,10 @@
+var i = 0;
+    var name = "Joe";
+    while (i < 2) {
+        document.write ("Happy Birthday to you. <br>");
+        i+=1;
+        
+    }
+
+    document.write ("Happy Birthday dear " + name + "<br>");
+    document.write ("Happy Birthday to you. <br>");
